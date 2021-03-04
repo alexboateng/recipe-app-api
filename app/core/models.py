a5954fdbbf24d20adfeb66a0ae33b09918747876
xelahaippa <xelahaippa@users.noreply.github.com>
2021-03-04 11:37:08 +0000

    add working list tags endpoint

--- app/core/models.py
+++ app/core/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
+from django.conf import settings
+
 
 class UserManager(BaseUserManager):
     """Manager for user profile"""
@@ -55,3 +57,15 @@ class User(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         """Return string representation of user"""
         return self.email
+
+class Tag(models.Model):
+    """Tag to be used for recipe"""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete = models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
+
